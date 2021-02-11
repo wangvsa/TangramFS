948ddac6ff67faf78c7a932cc01f80a0c8dea171
Chen Wang <wangvsa@gmail.com>
2021-02-11 10:50:55 -0600

    Update the interval insert function to consider more complexed cases

--- include/tangramfs-interval-tree.h
+++ include/tangramfs-interval-tree.h
@@ -4,6 +4,13 @@
 #include <stdbool.h>
 #include "utlist.h"
 
+#define IT_NO_OVERLAP       0
+#define IT_COVERED_BY_ONE   1
+#define IT_COVERS_ALL       2
+#define IT_PARTIAL_COVERED  3
+
+
+
 typedef struct Interval_t {
     size_t offset;               // offset of the target file
     size_t count;                // count of bytes
@@ -28,12 +35,93 @@ void tfs_it_destroy(IntervalTree *it) {
     }
 }
 
-void tfs_it_insert(IntervalTree* it, size_t offset, size_t count, size_t local_offset) {
-    Interval *interval = malloc(sizeof(Interval));
+inline bool is_overlap(Interval *i1, Interval *i2) {
+    if((i1->offset+i1->count > i2->offset) &&
+            (i1->offset <= i2->offset))
+        return true;
+    if((i2->offset+i2->count > i1->offset) &&
+            (i2->offset <= i1->offset))
+        return true;
+    return false;
+}
+
+// Return if i1 is fully covered by i2
+inline bool is_covered(Interval *i1, Interval *i2) {
+    if((i2->offset <= i1->offset) &&
+        (i2->offset+i2->count >= i1->offset+i1->count) )
+        return true;
+    return false;
+}
+
+void tfs_it_delete(IntervalTree* it, Interval* interval) {
+    LL_DELETE(it->head, interval);
+    free(interval);
+}
+
+void tfs_it_insert(IntervalTree* it, Interval* interval) {
+    LL_PREPEND(it->head, interval);
+}
+
+Interval* tfs_it_new(size_t offset, size_t count, size_t local_offset) {
+    Interval* interval = malloc(sizeof(Interval));
     interval->offset = offset;
     interval->count = count;
     interval->local_offset = local_offset;
-    LL_PREPEND(it->head, interval);
+    return interval;
+}
+
+Interval** tfs_it_overlaps(IntervalTree* it, Interval* interval, int *res, int *num_overlaps) {
+    *num_overlaps = 0;
+    Interval *i1, *i2;
+    i2 = interval;
+    LL_FOREACH(it->head, i1) {
+        *num_overlaps += is_overlap(i1, i2);
+    }
+
+    if(*num_overlaps == 0) {
+        *res = IT_NO_OVERLAP;
+        return NULL;
+    }
+
+    // Do a second pass to retrive all overlaps
+    // We are sure the intervals in the interval tree will have no overlaps.
+    Interval *current = i2;
+    Interval **overlaps = malloc(sizeof(Interval*) * (*num_overlaps));
+    int i = 0;
+    LL_FOREACH(it->head, i1) {
+        if(is_overlap(i1, i2))
+            overlaps[i++] = i1;
+    }
+
+    // 1. Only one overlap and it fully covers the current one
+    //    No need to insert the new interval, just update the old content
+    if(*num_overlaps == 1) {
+        if(is_covered(current, overlaps[0])) {
+            *res = IT_COVERED_BY_ONE;
+            return overlaps;
+        }
+    }
+
+    // 2. Fully covers multiple old ones
+    //    Delete all old ones and insert the new one
+    bool covers_all = true;
+    for(i = 0; i < *num_overlaps; i++) {
+        if(!is_covered(overlaps[i], current)) {
+            covers_all = false;
+            break;
+        }
+    }
+    if(covers_all) {
+        *res = IT_COVERS_ALL;
+        return overlaps;
+    }
+
+    // 3. Partial covered
+    //    Merge them to create a new interval, then delete
+    //    the old ones.
+    //
+    *res = IT_PARTIAL_COVERED;
+    return overlaps;
 }
 
 bool tfs_it_query(IntervalTree *it, size_t offset, size_t count, size_t *local_offset) {
--- src/tangramfs.c
+++ src/tangramfs.c
@@ -40,8 +40,63 @@ TFILE* tfs_open(const char* pathname, const char* mode) {
 }
 
 void tfs_write(TFILE* tf, void* buf, size_t count, size_t offset) {
-    tfs_it_insert(tf->it, offset, count, ftell(tf->local_file));
-    fwrite(buf, 1, count, tf->local_file);
+
+    int res, num_overlaps, i;
+
+    fseek(tf->local_file, 0, SEEK_END);
+    Interval *interval = tfs_it_new(offset, count, ftell(tf->local_file));
+    Interval** overlaps = tfs_it_overlaps(tf->it, interval, &res, &num_overlaps);
+
+    Interval *old, *start, *end;
+
+    switch(res) {
+        // 1. No overlap
+        // Write the data at the end of the local file
+        // Insert the new interval
+        case IT_NO_OVERLAP:
+            tfs_it_insert(tf->it, interval);
+            fwrite(buf, 1, count, tf->local_file);
+            break;
+        // 2. Have exactly one overlap
+        // The old interval fully covers the new one
+        // Only need to update the old content
+        case IT_COVERED_BY_ONE:
+            old = overlaps[0];
+            size_t local_offset = old->local_offset+(offset - old->offset);
+            fwrite(buf, 1, count, tf->local_file);
+            free(interval);
+            break;
+        // 3. The new interval fully covers several old ones
+        // Delete all old intervals and insert this new one
+        case IT_COVERS_ALL:
+            for(i = 0; i < num_overlaps; i++)
+                tfs_it_delete(tf->it, overlaps[i]);
+            tfs_it_insert(tf->it, interval);
+            fwrite(buf, 1, count, tf->local_file);
+            break;
+        case IT_PARTIAL_COVERED:
+            printf("IT_PARTIAL_COVERED: Not handled\n");
+            /*
+            Interval *start_interval = interval;
+            Interval *end_interval = interval;
+            size_t start_offset = interval->offset;
+            size_t end_offset = interval->offset + interval->count;
+            for(i = 0; i < *num_overlaps; i++) {
+                if(overlaps[i]->offset < start_offset) {
+                    start_offset = overlaps[i]->offset;
+                    start_interval = overlaps[i];
+                } else if(overlaps[i]->offset+overlaps[i]->count > end_offset) {
+                    end_offset = overlaps[i]->offset+overlaps[i]->count;
+                    //end_interval = overlaps[i];
+                }
+            }
+            */
+            break;
+    }
+
+    if(overlaps)
+        free(overlaps);
+
     fsync(fileno(tf->local_file));
 }
 
@@ -66,6 +121,7 @@ void tfs_close(TFILE* tf) {
     fclose(tf->local_file);
     tfs_it_destroy(tf->it);
 
+    free(tf->it);
     free(tf);
     tf = NULL;
 }
