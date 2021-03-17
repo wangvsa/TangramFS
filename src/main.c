1afe427e2a9d801c099920e754e3472597dbe787
Chen Wang <wangvsa@gmail.com>
2021-03-16 17:13:33 -0700

    Calculate the bandwidth more accurately

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
 
 #define MB (1024*1024)
 
-static int DATA_SIZE = 1*MB;
-static int N = 4;
+static int DATA_SIZE = 4*MB;
+static int N = 16;
 
 
 int size, rank, provided;
@@ -18,31 +18,33 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    //tfs_init("./", "/l/ssd");
-    tfs_init("./chen/", "/tmp");
+    tfs_init("./", "/l/ssd");
+    //tfs_init("./chen/", "/tmp");
 
     TFS_File* tf = tfs_open("./test.txt");
 
-    MPI_Barrier(MPI_COMM_WORLD);
-
 
     char* data = malloc(sizeof(char)*DATA_SIZE);
-    double tstart = MPI_Wtime();
-    int i;
-
     size_t offset = rank*DATA_SIZE*N;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double tstart = MPI_Wtime();
+
+    int i;
     tfs_seek(tf, offset, SEEK_SET);
     for(i = 0; i < N; i++)
         tfs_write(tf, data, DATA_SIZE);
     // Post all writes in one RPC
     tfs_post(tf, offset, N*DATA_SIZE);
-
-    MPI_Barrier(MPI_COMM_WORLD);
     double tend = MPI_Wtime();
 
+    double min_tstart, max_tend;
+    MPI_Reduce(&tstart, &min_tstart, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&tend, &max_tend, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
+
     if(rank == 0) {
-        printf("Total write size: %d MB, elapsed time: %fs\n", DATA_SIZE/MB*N*size, (tend-tstart));
-        printf("Bandwidth: %.2f MB/s\n", DATA_SIZE/MB*size*N/(tend-tstart));
+        printf("Total write size: %d MB, elapsed time: %fs\n", DATA_SIZE/MB*N*size, (max_tend-min_tstart));
+        printf("Bandwidth: %.2f MB/s\n", DATA_SIZE/MB*size*N/(max_tend-min_tstart));
     }
 
     // read neighbor rank's data
