ccdaf55333a5a69c83c0528798e45031863a8d95
Chen Wang <wangvsa@gmail.com>
2021-09-10 14:12:34 -0500

    Add script to install ucx

---
+++ .github/workflows/install-deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget https://github.com/openucx/ucx/releases/download/v1.11.1/ucx-1.11.1.tar.gz
+
+tar -zxf ./ucx-1.11.1.tar.gz
+
+cd ./ucx-1.11.1 && mkdir install
+
+export UCX_DIR=`pwd`/instal
+echo $UCX_DIR
+
+./autogen.sh
+./contrib/configure-release --prefix=$UCX_DIR --without-cuda --without-java
+make -j8
+make install
