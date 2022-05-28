47fd31089ab0d2c11dbdf959a9a54854e2afaec9
Chen Wang <wangvsa@gmail.com>
2022-05-28 11:46:19 -0500

    Allow cmd args to change num accesses, access size, write mode, and read mode

--- examples/iobench.c
+++ examples/iobench.c
@@ -3,22 +3,33 @@
 #include <execinfo.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
 #include "mpi.h"
 
+#define WRITE_MODE_STRIDED    "strided"
+#define WRITE_MODE_CONTIGUOUS "contiguous"
+#define READ_MODE_CONTIGUOUS  "contiguous"
+#define READ_MODE_RANDOM      "random"
+
 #define MB (1024*1024)
 #define KB (1024)
 
 #define FILENAME "./test.txt"
 
-// Can be modified by input arguments
-static size_t DATA_SIZE = 4*MB;
-static int    N = 10;
 
-int mpi_size, mpi_rank;
+// Can be modified by input arguments
+static char   write_mode[20];
+static char   read_mode[20];
+static size_t access_size;      // in KB
+static int    num_accesses;
+
+
+int mpi_size;
+int mpi_rank;
 
 // Final output result
 static double write_tstart, write_tend;
@@ -32,14 +43,14 @@ void write_contiguous() {
 
     FILE* fp = fopen(FILENAME, "wb");
 
-    char* data = malloc(sizeof(char)*DATA_SIZE);
-    size_t offset = mpi_rank*DATA_SIZE*N;
+    char* data = malloc(sizeof(char)*access_size);
+    size_t offset = mpi_rank*access_size*num_accesses;
     fseek(fp, offset, SEEK_SET);
 
     MPI_Barrier(MPI_COMM_WORLD);
     write_tstart = MPI_Wtime();
-    for(int i = 0; i < N; i++) {
-        fwrite(data, 1, DATA_SIZE, fp);
+    for(int i = 0; i < num_accesses; i++) {
+        fwrite(data, 1, access_size, fp);
     }
     MPI_Barrier(MPI_COMM_WORLD);
     write_tend = MPI_Wtime();
@@ -52,15 +63,15 @@ void write_strided() {
     FILE* fp = fopen(FILENAME, "wb");
 
     size_t offset;
-    char* data = malloc(sizeof(char)*DATA_SIZE);
+    char* data = malloc(sizeof(char)*access_size);
 
     MPI_Barrier(MPI_COMM_WORLD);
     write_tstart = MPI_Wtime();
 
-    for(int i = 0; i < N; i++) {
-        size_t offset = mpi_size*DATA_SIZE*i + mpi_rank*DATA_SIZE;
+    for(int i = 0; i < num_accesses; i++) {
+        size_t offset = mpi_size*access_size*i + mpi_rank*access_size;
         fseek(fp, offset, SEEK_SET);
-        fwrite(data, 1, DATA_SIZE, fp);
+        fwrite(data, 1, access_size, fp);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
@@ -73,15 +84,15 @@ void write_strided() {
 void read_contiguous() {
     FILE* fp = fopen(FILENAME, "rb");
 
-    char* data = malloc(sizeof(char)*DATA_SIZE);
+    char* data = malloc(sizeof(char)*access_size);
 
-    size_t offset = mpi_rank*DATA_SIZE*N;
+    size_t offset = mpi_rank*access_size*num_accesses;
     fseek(fp, offset, SEEK_SET);
 
     MPI_Barrier(MPI_COMM_WORLD);
     read_tstart = MPI_Wtime();
-    for(int i = 0; i < N; i++) {
-        fread(data, 1, DATA_SIZE, fp);
+    for(int i = 0; i < num_accesses; i++) {
+        fread(data, 1, access_size, fp);
     }
     MPI_Barrier(MPI_COMM_WORLD);
     read_tend = MPI_Wtime();
@@ -94,25 +105,56 @@ void read_random() {
     FILE* fp = fopen(FILENAME, "rb");
     struct stat st;
 
-    char* data = malloc(sizeof(char)*DATA_SIZE);
+    char* data = malloc(sizeof(char)*access_size);
 
     time_t t;
     srand((unsigned) time(&t));
-    int num_blocks = N * mpi_size;
+    int num_blocks = num_accesses * mpi_size;
     size_t offset;
 
     MPI_Barrier(MPI_COMM_WORLD);
     read_tstart = MPI_Wtime();
-    for(int i = 0; i < N; i++) {
-        offset = (rand() % num_blocks) * DATA_SIZE;
+    for(int i = 0; i < num_accesses; i++) {
+        offset = (rand() % num_blocks) * access_size;
         fseek(fp, offset, SEEK_SET);
-        fread(data, 1, DATA_SIZE, fp);
+        fread(data, 1, access_size, fp);
     }
     MPI_Barrier(MPI_COMM_WORLD);
     read_tend = MPI_Wtime();
 
     free(data);
     fclose(fp);
+}
+
+void init_args() {
+    strcpy(write_mode, "");
+    strcpy(read_mode, "");
+    access_size  = 4*KB;
+    num_accesses = 10;
+}
+
+void parse_cmd_args(int argc, char* argv[]) {
+    int opt;
+    while((opt = getopt(argc, argv, ":w:r:s:n:")) != -1) {
+        switch(opt) {
+            case 'w':
+                strcpy(write_mode, optarg);
+                break;
+            case 'r':
+                strcpy(read_mode, optarg);
+                break;
+            case 's':
+                access_size = atol(optarg) * KB;
+                break;
+            case 'n':
+                num_accesses = atoi(optarg);
+                break;
+            case '?':
+                printf("Unknown option: %c\n", optopt);
+                break;
+        }
+    }
+    printf("Write mode: %s, Read mode: %s, Access size: %ldKB, Num accesses: %d\n", write_mode, read_mode, access_size, num_accesses);
 }
 
 int main(int argc, char* argv[]) {
@@ -122,34 +164,36 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
     if(mpi_rank == 0) {
-        if(argc == 3) {
-            N = atoi(argv[1]);
-            DATA_SIZE = atoi(argv[2]) * KB;
-        }
+        init_args();
+        parse_cmd_args(argc, argv);
     }
-    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&DATA_SIZE, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&write_mode,  20, MPI_BYTE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&read_mode,   20, MPI_BYTE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&num_accesses, 1, MPI_INT,  0, MPI_COMM_WORLD);
+    MPI_Bcast(&access_size,  1, MPI_LONG, 0, MPI_COMM_WORLD);
 
-    for(int i = 0; i < 1; i++) {
-        MPI_Barrier(MPI_COMM_WORLD);
-        //write_contiguous();
+    // Write phase
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(strcmp(write_mode, WRITE_MODE_CONTIGUOUS) == 0)
+        write_contiguous();
+    if(strcmp(write_mode, WRITE_MODE_STRIDED) == 0)
         write_strided();
-    }
 
-    for(int i = 0; i < 1; i++) {
-        MPI_Barrier(MPI_COMM_WORLD);
-        //read_contiguous();
-        //read_random();
-    }
+    // Read phase
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(strcmp(read_mode, READ_MODE_CONTIGUOUS) == 0)
+        read_contiguous();
+    if(strcmp(read_mode, READ_MODE_RANDOM) == 0)
+        read_random();
 
     MPI_Barrier(MPI_COMM_WORLD);
     if(mpi_rank == 0) {
 
-        write_iops = N * mpi_size / (write_tend-write_tstart);
-        write_bandwidth = DATA_SIZE / (DATA_SIZE>=MB?MB:KB) * N * mpi_size / (write_tend-write_tstart);
+        write_iops = num_accesses * mpi_size / (write_tend-write_tstart);
+        write_bandwidth = access_size / (access_size>=MB?MB:KB) * num_accesses * mpi_size / (write_tend-write_tstart);
 
-        read_iops = N * mpi_size / (read_tend-read_tstart);
-        read_bandwidth  = DATA_SIZE / (DATA_SIZE>=MB?MB:KB) * N * mpi_size / (read_tend-read_tstart);
+        read_iops = num_accesses * mpi_size / (read_tend-read_tstart);
+        read_bandwidth  = access_size / (access_size>=MB?MB:KB) * num_accesses * mpi_size / (read_tend-read_tstart);
 
         printf("Write IOPS: %8d, Bandwidth: %.3f\t\tRead IOPS: %d, Bandwidth: %.3f\n", write_iops, write_bandwidth, read_iops, read_bandwidth);
         fflush(stdout);
