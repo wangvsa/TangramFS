2a22c30408f0125f528fe7a44e1fd991327188f1
Chen Wang <wangvsa@gmail.com>
2023-01-12 15:59:21 -0800

    Add comments

--- include/tangramfs-delegator.h
+++ include/tangramfs-delegator.h
@@ -3,6 +3,12 @@
 
 #include "tangramfs.h"
 
+/**
+ * Delegator is used for locking-based implementation.
+ * Rank 0 of each node serves as the node-local server
+ * and processes all requests from the same node.
+ */
+
 void tangram_delegator_start(tfs_info_t* tfs_info);
 void tangram_delegator_stop();
 
